f993c5ecbd6661f9818b68b821e6573eb26db172
JGL89 <julianag403@gmail.com>
2022-04-28 21:16:43 -0500

    Conexión de la API Restful con MongoBD.

--- 12/backend/src/server.js
+++ 12/backend/src/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const server = express();
 const port = 4000;
+
+mongoose
+  .connect(
+    'mongodb+srv://JuliDev:031789bb@cluster0.9d1qn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+    .then(() =>console.log('conectado con la base de datos'))
+    .catch((err) =>
+    console.log('Error al conectarse a la base de datos, erro: ' + err)
+    );
 
 server.get('/', (request, response) => {
     response.send('Hola desde la raíz');
